be17f5155b7661ce480336439e6b1071772f6bc2
ather ali <a.ngaming102734@gmail.com>
2026-02-13 17:59:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+// // window.location.replace("https://www.youtube.com/");
+// window.location.reload(true);
+// window.location.reload(false);
+// window.location.reload();
+// function sum1(a,b,c, ..) {
+// }
+// function sum(a,...number) {
+//     console.log(a,number);
+// }
+// // sum(1,
+// let arr =[1,2,3,4]
+// let arr1 = [12,13,14]
+// sum(1,arr,arr1)
+// var arr = ['']
+// let arr = [1,2,3,4,5,6];
+// let sumNumber = arr .reduce((acc) =>{
+//       return acc +current
+// })
+// console.log(sumNumber+arr)
+// function sum1(a,b,c,... numbers) {
+//     var sum = 'a + b + c, ...number';
+//     var sum2 =('1,2,3,...number')
+//     let sumnum = 4     
+// }
+// REST OPRATOR
+
+// ==============================================================================
+
+// WITHOUT REST OPRATOR
+
+// function arr() {
+//     let arr = [1, 2, 3, 4, 5, 6,];
+//     let sumNumber = arr.reduce(function(a, b) {
+//         return a + b;
+//     });;
+//     return sumNumber;
+// }
+// console.log(arr());
+// // let sum = arr.reduce((a, b) => {
+//     return a + b;
+// });
+
+
+// ==============================================================================
+
+// WITH REST OPRATOR/PARAMETORS
+// function sum(a,...numbers) {
+//     console.log('a, numbers:', a, numbers); // WILL RECEVIE ARRAY
+// }
+// let arr = [1, 2, 4];
+// let arr1 = [12, 14, 15]
+// sum(1, arr, arr1);
+// let arr = [1, 2, 3, 4, 5, 6,];
+// let sumNumber = arr.reduce((accumlated, current) => {
+//     return accumlated + current;
+// });
